89b76d88f428afe0ffd875e2a8e63b29d24a4c91
John Pradeep <john.pradeep@tui.co.uk>
2020-06-27 20:57:18 +0100

    cleanup client interface

--- penman/src/lib.rs
+++ penman/src/lib.rs
@@ -1,9 +1,25 @@
 mod store;
 use api::{ledger_api_client::LedgerApiClient, CreateLedgerRequest, LedgerCreatedResponse};
+use store::*;
 
-pub async fn create_ledger(port: u16) -> api::Result<String> {
-    let mut client = LedgerApiClient::connect(format!("http://[::1]:{}", port)).await?;
-    let request = tonic::Request::new(CreateLedgerRequest {});
-    let response = client.create(request).await?;
-    Ok(response.into_inner().ledger_id)
+pub struct Penman {
+    store: Store,
+}
+
+impl Penman {
+    pub async fn new(etcd: Vec<String>) -> api::Result<Penman> {
+        let store = Store::new(etcd).await;
+        Ok(Penman { store })
+    }
+
+    pub async fn create_ledger(&self, port: u16) -> api::Result<String> {
+        let mut client = LedgerApiClient::connect(format!("http://[::1]:{}", port)).await?;
+        let request = tonic::Request::new(CreateLedgerRequest {});
+        let response = client.create(request).await?;
+        Ok(response.into_inner().ledger_id)
+    }
+}
+
+pub async fn new(etcd: Vec<String>) -> api::Result<Penman> {
+    Penman::new(etcd).await
 }
--- penman/tests/integration.rs
+++ penman/tests/integration.rs
@@ -3,8 +3,10 @@ mod utils;
 #[tokio::test]
 async fn create_new_ledger() {
     let (tx, port) = utils::start_server().await;
+    let etcd = vec!["http://localhost:2379".to_owned()];
+    let penman = penman::new(etcd).await.unwrap();
 
-    let ledger_id = penman::create_ledger(port).await.unwrap();
+    let ledger_id = penman.create_ledger(port).await.unwrap();
 
     assert_eq!(ledger_id.len(), 36);
     tx.send(()).unwrap();
