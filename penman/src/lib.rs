a2d7d2b213e19398d3ca605572a22ee42101eb2c
John Pradeep <john.pradeep@tui.co.uk>
2020-06-28 17:04:37 +0100

    clean up penman api

--- penman/src/lib.rs
+++ penman/src/lib.rs
@@ -1,32 +1,34 @@
 mod store;
+mod types;
 use api::{ledger_api_client::LedgerApiClient, CreateLedgerRequest};
 use store::*;
 use tonic::transport::channel::Channel;
+use types::*;
 
 pub struct Penman {
     store: Store,
-    ledgers: Vec<LedgerApiClient<Channel>>,
+    clients: Vec<LedgerApiClient<Channel>>,
 }
 
 impl Penman {
-    pub async fn new(etcd: Vec<String>) -> api::Result<Penman> {
-        let mut ledgers = vec![];
+    pub async fn new(etcd: Vec<String>) -> Result<Penman> {
+        let mut clients = vec![];
         let store = Store::new(etcd).await;
         for endpoint in store.get_prefix("ledgers.").await {
             println!("url: {}", endpoint);
-            ledgers.push(LedgerApiClient::connect(endpoint).await?);
+            clients.push(LedgerApiClient::connect(endpoint).await?);
         }
-        Ok(Penman { store, ledgers })
+        Ok(Penman { store, clients })
     }
 
-    pub async fn create_ledger(&mut self) -> api::Result<String> {
-        let client = self.ledgers.get_mut(0).unwrap();
+    pub async fn create_ledger(&mut self) -> Result<String> {
+        let client = self.clients.get_mut(0).unwrap();
         let request = tonic::Request::new(CreateLedgerRequest {});
         let response = client.create(request).await?;
         Ok(response.into_inner().ledger_id)
     }
 }
 
-pub async fn new(etcd: Vec<String>) -> api::Result<Penman> {
+pub async fn new(etcd: Vec<String>) -> Result<Penman> {
     Penman::new(etcd).await
 }
---
+++ penman/src/types.rs
@@ -0,0 +1,19 @@
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    TranportError(tonic::transport::Error),
+    RpcError(tonic::Code),
+}
+
+impl std::convert::From<tonic::transport::Error> for Error {
+    fn from(e: tonic::transport::Error) -> Self {
+        Error::TranportError(e)
+    }
+}
+
+impl std::convert::From<tonic::Status> for Error {
+    fn from(status: tonic::Status) -> Self {
+        Error::RpcError(status.code())
+    }
+}
